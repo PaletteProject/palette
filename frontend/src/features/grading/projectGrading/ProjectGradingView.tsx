92dd04c01ab35e0ede87539e41e02c574365a0cb
jake <jsmith181997@gmail.com>
2025-01-28 11:16:09 -0600

    chore: test display of group member info and criteria
    - #197

--- frontend/src/features/grading/projectGrading/ProjectGradingView.tsx
+++ frontend/src/features/grading/projectGrading/ProjectGradingView.tsx
@@ -27,20 +27,39 @@ export function ProjectGradingView({
           "scroll-auto fixed z-80 inset-0 bg-black bg-opacity-75 flex justify-center items-center text-white"
         }
       >
-        <p>{groupName}</p>
-        <div>
-          {submissions.map((submission: Submission) => (
-            <p key={submission.id}>{submission.user.asurite}</p>
-          ))}
-
-          {rubric.criteria.map((criteria: Criteria) => (
-            <div key={criteria.key}>
-              <p>{criteria.description}</p>
-            </div>
-          ))}
+        <div className="bg-gray-700 p-6 rounded-xl shadow-lg relative w-1/2">
+          <h1 className="text-4xl text-white font-semibold mb-3">
+            {groupName}
+          </h1>
+          {renderGroupMembers()}
+          {renderCriteria()}
         </div>
       </div>,
       document.getElementById("portal-root") as HTMLElement,
+    );
+  };
+
+  const renderGroupMembers = () => {
+    return (
+      <div>
+        {submissions.map((submission: Submission) => (
+          <div key={submission.id} className={"mb-4"}>
+            <h2>{`${submission.user.name} (${submission.user.asurite})`}</h2>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+  const renderCriteria = () => {
+    return (
+      <div>
+        {rubric.criteria.map((criterion: Criteria) => (
+          <div key={criterion.key}>
+            <h2>{criterion.description}</h2>
+          </div>
+        ))}
+      </div>
     );
   };
 
