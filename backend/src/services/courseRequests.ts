298e6a5e6531df43d46897a5d9b012299554113e
jake <jsmith181997@gmail.com>
2025-02-06 15:45:18 -0600

    refactor: extract pagination logic to helper functions
    - move outside API logic
    - #240

--- backend/src/services/courseRequests.ts
+++ backend/src/services/courseRequests.ts
@@ -29,6 +29,46 @@ type GradedSubmission = {
 };
 
 const RESULTS_PER_PAGE = 100;
+
+/**
+ * Helper for handling course pagination from the Canvas API.
+ */
+async function getAllCourses() {
+  let canvasCourses: CanvasCourse[] = [];
+  let page = 1;
+  let fetchedCourses: CanvasCourse[];
+
+  do {
+    fetchedCourses = await fetchAPI<CanvasCourse[]>(
+      `/courses?per_page=${RESULTS_PER_PAGE}&page=${page}`,
+    );
+
+    canvasCourses = canvasCourses.concat(fetchedCourses);
+    page++;
+  } while (fetchedCourses.length === RESULTS_PER_PAGE); // continue if we received a full page
+
+  return canvasCourses;
+}
+
+/**
+ * Helper for handling assignment pagination from the Canvas API.
+ */
+async function getAllAssignments(courseId: string) {
+  let canvasAssignments: CanvasAssignment[] = [];
+  let page = 1;
+  let fetchedAssignments: CanvasAssignment[];
+
+  do {
+    fetchedAssignments = await fetchAPI<CanvasAssignment[]>(
+      `/courses/${courseId}/assignments?per_page=${RESULTS_PER_PAGE}&page=${page}`,
+    );
+    canvasAssignments = canvasAssignments.concat(fetchedAssignments);
+    page++;
+  } while (fetchedAssignments.length === RESULTS_PER_PAGE);
+
+  return canvasAssignments;
+}
+
 /**
  * Defines CRUD operations for courses from the Canvas API.
  */
@@ -38,21 +78,8 @@ export const CoursesAPI = {
    *
    * @returns Promise for a filtered array of course objects.
    */
-
   async getCourses(): Promise<Course[]> {
-    let canvasCourses: CanvasCourse[] = [];
-    let page = 1;
-    let fetchedCourses: CanvasCourse[];
-
-    do {
-      fetchedCourses = await fetchAPI<CanvasCourse[]>(
-        `/courses?per_page=${RESULTS_PER_PAGE}&page=${page}`,
-      );
-
-      canvasCourses = canvasCourses.concat(fetchedCourses);
-      page++;
-    } while (fetchedCourses.length === RESULTS_PER_PAGE); // continue if we received a full page
-
+    const canvasCourses = await getAllCourses();
     const oneYearAgo = new Date();
     oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
     console.log(oneYearAgo);
@@ -86,9 +113,7 @@ export const CoursesAPI = {
     if (!courseId) {
       throw new Error("Course ID is undefined");
     }
-    const canvasAssignments = await fetchAPI<CanvasAssignment[]>(
-      `/courses/${courseId}/assignments?per_page=${RESULTS_PER_PAGE}`,
-    );
+    const canvasAssignments = await getAllAssignments(courseId);
     return canvasAssignments.map(mapToPaletteAssignment);
   },
 
