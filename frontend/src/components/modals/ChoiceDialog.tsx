d7e20cfbc7d678ed5917941a94ed097e226376fa
jake <jsmith181997@gmail.com>
2025-02-10 16:34:50 -0600

    fix: prettier

--- frontend/src/components/modals/ChoiceDialog.tsx
+++ frontend/src/components/modals/ChoiceDialog.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { ButtonColorOptions, PaletteActionButton } from "../buttons/PaletteActionButton.tsx";
+import {
+  ButtonColorOptions,
+  PaletteActionButton,
+} from "../buttons/PaletteActionButton.tsx";
 
 /**
  * A choice is a button that the user can click to perform an action.
