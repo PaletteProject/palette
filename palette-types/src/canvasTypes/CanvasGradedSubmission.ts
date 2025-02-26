e09e84b1a6e6557acf59d2c3197822e925df87c8
jake <jsmith181997@gmail.com>
2025-02-26 13:42:09 -0600

    chore: update CanvasGradedSubmissions type to support comments
    - #254 #255

--- palette-types/src/canvasTypes/CanvasGradedSubmission.ts
+++ palette-types/src/canvasTypes/CanvasGradedSubmission.ts
@@ -1,7 +1,15 @@
+/**
+ * What Canvas needs to properly update a submission.
+ */
+
 export type CanvasGradedSubmission = {
   submission_id: number;
   user: { id: number; name: string; asurite: string };
   rubric_assessment: {
     [p: string]: { points: number; rating_id: string; comments: string };
+  };
+  comment?: {
+    text_comment: string;
+    group_comment: boolean;
   };
 };
