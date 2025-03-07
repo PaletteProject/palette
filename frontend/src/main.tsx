3f52260f84bb9c25f43279271310fa8e79dff21d
Ben Aspinall <benjaminaspinall1@gmail.com>
2025-03-06 21:16:46 -0700

    chore: add strict mode back

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,11 @@
 Entry point for the entire application.
  */
 
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
 import {
   GradingMain,
   Home,
@@ -22,22 +24,24 @@ import TemplatesMain from "./features/templatesPage/TempatesMain.tsx";
 
 // Defined a "root" div in index.html that we pull in here and then call the React render method.
 createRoot(document.getElementById("root")!).render(
-  <CourseProvider>
-    <AssignmentProvider>
-      <RubricProvider>
-        <DialogProvider>
-          <Router>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/templates" element={<TemplatesMain />} />
-              <Route path="/rubric-builder" element={<RubricBuilderMain />} />
-              <Route path="/grading" element={<GradingMain />} />
-              <Route path="/settings" element={<SettingsMain />} />
-              <Route path={"*"} element={<NotFoundPage />} />
-            </Routes>
-          </Router>
-        </DialogProvider>
-      </RubricProvider>
-    </AssignmentProvider>
-  </CourseProvider>,
+  <StrictMode>
+    <CourseProvider>
+      <AssignmentProvider>
+        <RubricProvider>
+          <DialogProvider>
+            <Router>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/templates" element={<TemplatesMain />} />
+                <Route path="/rubric-builder" element={<RubricBuilderMain />} />
+                <Route path="/grading" element={<GradingMain />} />
+                <Route path="/settings" element={<SettingsMain />} />
+                <Route path={"*"} element={<NotFoundPage />} />
+              </Routes>
+            </Router>
+          </DialogProvider>
+        </RubricProvider>
+      </AssignmentProvider>
+    </CourseProvider>
+  </StrictMode>
 );
