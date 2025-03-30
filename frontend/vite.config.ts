f10b180d59d80b697551d38e42a22e2470d83232
jake <jsmith181997@gmail.com>
2025-03-30 09:19:36 -0500

    dev: split dockerfiles into front and backend services
    - remove old aliases from tsconfig
    - remove old test config from vite config
    
    - #357

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { configDefaults } from "vitest/config";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "/src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   base: "./",
@@ -18,15 +17,5 @@ export default defineConfig({
   server: {
     host: "0.0.0.0", // listen on all network interfaces not just localhost for 5173 to work with docker container
     port: 5173,
-  },
-  test: {
-    ...configDefaults,
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/setupTests.ts",
-    coverage: {
-      reportsDirectory: "./coverage",
-      reporter: ["text", "json", "html"],
-    },
   },
 });
