c4e4a96c356dca5621fea9caf415743a422b4492
benaspinall1 <benjaminaspinall1@gmail.com>
2025-04-11 23:12:41 -0700

    feature: exclude metrics from ci

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,9 +1,18 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/dist/", "/node_modules/"], // Don't run tests in these directories
-  // tell jest to interpret .js-ending files as .ts files during testing
+  extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        useESM: true,
+      },
+    ],
+  },
+  testPathIgnorePatterns: ["rubrics.test.ts", "submissions.test.ts"],
 };
